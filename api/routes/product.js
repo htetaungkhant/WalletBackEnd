35f708c04a2a3f79d81b33ab5e9497815cca3462
htetaungkhant <chitkogyi19950@gmail.com>
2019-04-10 20:40:29 +0630

    confirm

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,40 +1,12 @@
 const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
-const multer=require('multer');
-
-const storage=multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,'./uploads');
-    },
-    filename: (req,file,cb)=>{
-        //toISOString()
-        cb(null, new Date().getTime()+file.originalname);
-    }
-});
-
-const fileFilter=(req,file,cb)=>{
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-        cb(null,true);  
-    }
-    else{
-        cb(null,false);
-    }
-};
-
-const upload=multer({
-    storage: storage,
-    limits:{
-        fileSize: 1024*1024*5
-    },
-    fileFilter: fileFilter
-});
 
 const Product= require('../models/product');
 
 router.get('/',(req,res,next)=>{
     Product.find()
-    .select("name price _id productImage")
+    .select("name price _id")
     .exec()
     .then(docs =>{
         const response={
@@ -43,7 +15,6 @@ router.get('/',(req,res,next)=>{
                 return{
                     name: doc.name,
                     price: doc.price,
-                    productImage: doc.productImage,
                     _id: doc._id,
                     request:{
                         type: 'GET',
@@ -69,12 +40,11 @@ router.get('/',(req,res,next)=>{
     });
 });
 
-router.post('/',upload.single('productImage'),(req,res,next)=>{
+router.post('/',(req,res,next)=>{
     const product=new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price,
-        productImage: req.file.path
+        price: req.body.price
     });
     product
     .save()
@@ -105,7 +75,7 @@ router.post('/',upload.single('productImage'),(req,res,next)=>{
 router.get('/:productId',(req,res,next)=>{
     const id=req.params.productId;
    Product.findById(id)
-   .select('name price _id productImage')
+   .select('name price _id')
    .exec()
    .then(doc=>{
        console.log("From database",doc);

fbe305508bd9a68b4f17e27f5d52265a99d64192
htetaungkhant <chitkogyi19950@gmail.com>
2019-04-10 21:06:48 +0630

    confirm

--- api/models/product.js
+++ api/models/product.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose');
 const productSchema=mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true},
-    price: { type: Number, required: true}
+    price: { type: Number, required: true},
+    quantity: { type: Number, default: 0}
 });
 
 module.exports=mongoose.model('Product',productSchema);
--- api/models/user.js
+++ api/models/user.js
@@ -8,7 +8,8 @@ const userSchema=mongoose.Schema({
         required: true ,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    balance: {type: Number, default: 0}
 });
 
 module.exports=mongoose.model('User',userSchema);
